8ec5450e2fc8ba0509c2cb51a0855a8288f0daf4
jokero <jokero@aliyun.com>
2020-08-09 17:00:46 +0800

    m.aliyun

---
+++ js/lunbo.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            clickableClass: 'my-pagination-clickable',
+            type: 'progressbar',
+            renderProgressbar: function(progressbarFillClass) {
+                return '<span class="' + progressbarFillClass + '"></span>';
+            }
+        },
+        autoplay: true,
+        loop: true,
+    });
+
+})
