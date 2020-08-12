d3997418cfc11ed374546231e3aac295d1572e1b
jokero <jokero@aliyun.com>
2020-08-12 20:39:48 +0800

    m_aliyun/change

--- js/m.aliyun.js
+++ js/m.aliyun.js
@@ -11,3 +11,17 @@
     window.addEventListener("resize", resizeBaseFontSize, false);
     window.addEventListener("orientationchange", resizeBaseFontSize, false);
 })();
+window.onload = function() {
+    let m_login = document.querySelector(".m_login");
+    let flag = true;
+    let m_user = document.getElementById("m_user");
+    m_user.onclick = function() {
+        if (!flag) {
+            m_login.style.display = "none";
+            flag = true;
+        } else {
+            m_login.style.display = "block";
+            flag = false;
+        }
+    }
+}
