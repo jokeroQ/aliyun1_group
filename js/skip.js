010717ca63a227f589d44874620dbbc41d9e17c3
demon <demon18779222075@aliyun.com>
2020-08-13 20:40:45 +0800

    skip

---
+++ js/skip.js
@@ -0,0 +1,43 @@
+function one(){
+    document.getElementById("one").style.display="block";
+    document.getElementById("two").style.display="none";
+}
+function two(){
+    document.getElementById("two").style.display="block";
+    document.getElementById("one").style.display="none";
+}
+function twoa(){
+    document.getElementById("three_one_one").style.display="block";
+    document.getElementById("three_two_one").style.display="none";
+    document.getElementById("three_three_one").style.display="none";
+    document.getElementById("three_four_one").style.display="none";
+    document.getElementById("three_five_one").style.display="none";
+}
+function twob(){
+    document.getElementById("three_one_one").style.display="none";
+    document.getElementById("three_two_one").style.display="block";
+    document.getElementById("three_three_one").style.display="none";
+    document.getElementById("three_four_one").style.display="none";
+    document.getElementById("three_five_one").style.display="none";
+}
+function twoc(){
+    document.getElementById("three_one_one").style.display="none";
+    document.getElementById("three_two_one").style.display="none";
+    document.getElementById("three_three_one").style.display="block";
+    document.getElementById("three_four_one").style.display="none";
+    document.getElementById("three_five_one").style.display="none";
+}
+function twod(){
+    document.getElementById("three_one_one").style.display="none";
+    document.getElementById("three_two_one").style.display="none";
+    document.getElementById("three_three_one").style.display="none";
+    document.getElementById("three_four_one").style.display="block";
+    document.getElementById("three_five_one").style.display="none";
+}
+function twoe(){
+    document.getElementById("three_one_one").style.display="none";
+    document.getElementById("three_two_one").style.display="none";
+    document.getElementById("three_three_one").style.display="none";
+    document.getElementById("three_four_one").style.display="none";
+    document.getElementById("three_five_one").style.display="block";
+}
